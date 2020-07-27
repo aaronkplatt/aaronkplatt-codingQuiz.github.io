2897c55910c910b13d87fd4a87a1cd8473e24c84
Aaron Platt <aaron.k.platt@gmail.com>
2020-07-26 20:10:21 -0700

    I've got most of the javascript working. i need to work on the highscore page still

--- Master/assets/js/quiz.js
+++ Master/assets/js/quiz.js
@@ -1,5 +1,119 @@
-//variables 
+//time
+var timerEl = document.getElementById("time");
+var time = 75000; //starting length 75 seconds
+var wrongAnswerPenalty = 15000; // 15 seconds deducted if chosen incorrectly
+var answerTimer;
+var quizTimer;
+var answerTimeOut = 0;
+ 
+//questions
+var questionCounter = 0;
+var questionTitle = document.getElementById("question-title"); 
+var questionChoices = document.getElementById("question-choices");
 
-//variables for DOM elements
+//other variables
+var startBtn = document.getElementById("start");
+var finalScore = document.getElementById("final-score");
 
+//screen elements
+var startScreenEl = document.getElementById("start-screen");
+var questionsEl = document.getElementById("questions");
+var endScreenEl = document.getElementById("end-screen");
+
+//result of answer (Correct/Wrong)
+var resultScreenEl = document.getElementById("result-screen");
+var resultText = document.getElementById("resultText");
+
+//Start (Btn on the bottom of this js file) this function starts the quiz by clicking the button and generating the questions to appear while also showing the timer beginning to count down.
+function startQuiz() {
+    updateTime(0); //sets initial time
+    startScreenEl.setAttribute("class", "hide"); 
+    questionsEl.removeAttribute("class");
+    questionCounter = 0;
+    getQuestions();    
+    quizTimer = setInterval(updateTime, 1000);
+}
+
+//this function generates the question and clears the choice from before. It also has a for loop that shows all of the choices for the designated question.
+function getQuestions() {
+    questionTitle.innerHTML = questions[questionCounter].title;
+    questionChoices.innerHTML = ""; // removes old choices
+
+    // looping the question choices and converting them into buttons and li. 
+    for (let i = 0; i < questions[questionCounter].choices.length; i++) {
+        var li = document.createElement("li");
+        var button = document.createElement("button");
+        var choice = questions[questionCounter].choices[i];
+        button.innerHTML = choice;
+        button.value = choice;
+    //on click function for the users choice.
+        button.addEventListener("click", function() {
+            answerClick(this.value);
+        });
+        li.appendChild(button);
+        questionChoices.appendChild(li);
+    };
+}
+//this function takes away the time if you get a question wrong and or moves onto the next question until you are out of questions, than it will send you to the end screen.
+function answerClick(x) {
+    var answer = questions[questionCounter].answer;
+    showAnswer(x == answer);
+    if (x !== answer) {
+        time -= wrongAnswerPenalty;
+        showTime();
+    }
+    questionCounter ++;
+    if (questionCounter < questions.length) {
+        getQuestions();
+    }
+    else {
+        showEndScreen();
+    }
+}
+
+//this function counts down the timer
+function updateTime() {
+    time -= 1000;
+    showTime();  
+}
+
+//this function counts down the time and or sends you to the end screen if the user runs out of time.
+function showTime() {
+    if (time <= 0) {
+        time = 0;
+        clearInterval(quizTimer);
+        showEndScreen();
+    }
+    //timer
+    timerEl.innerHTML = Math.ceil(time/1000); 
+}
+
+//this function show you that you where right or wrong than disapears after 2 seconds.
+function showAnswer(isCorrect) {
+    if (isCorrect) {
+        resultText.innerHTML = "Correct";
+    }
+    else {
+        resultText.innerHTML = "Wrong";
+    }
+    resultScreenEl.removeAttribute("class");
+    answerTimer = setTimeout(hideResultScreen, 2000);
+}
+
+//This screen tells you if you are right or wrong.
+function hideResultScreen() {
+    resultScreenEl.setAttribute("class", "hide");
+
+}
+
+//When you complete the quiz this function gets called and stops the timer which is your score
+function showEndScreen() {
+    clearInterval(quizTimer);
+    questionsEl.setAttribute("class", "hide");
+    endScreenEl.removeAttribute("class");
+    finalScore.innerHTML = time/1000;
+}
+
+//btn priorities
+startBtn.onclick = startQuiz;
 
